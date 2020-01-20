763c27aa0369cf987c272628f78c9f775e0e7198
Kevin Vanantwerpen <kevin.v1989@gmail.com>
2020-01-20 21:35:22 +0100

    setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+import TodoList from './components/Todo/TodoList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: []
+  }
+
+  getData = () =>{
+    axios.get('http://5de80f759578cb001487adea.mockapi.io/Todo')
+    .then(response => {
+      const { data } = response;
+      this.setState({todos: data})
+      // handle success
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+    });
+  }
+
+  componentDidMount(){
+    this.getData();
+  }
+
+  render() {
+    const { todos } = this.state;
+    console.log(todos);
+    return (
+      <div className="main">
+        <TodoList todoArray={todos}/>
+
+      </div>
+    );
+  }
 }
 
 export default App;
